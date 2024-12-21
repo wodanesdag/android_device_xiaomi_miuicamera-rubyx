caeacb5bf0d17b69abf7a8d8555226baaab948d3
wodanesdag <wodanesdag@proton.me>
2024-12-21 19:43:17 +0700

    Switch to py extract utils

---
+++ extract-files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/miuicamera-ruby',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
+        .apktool_patch('blob-patches/MIUICamera.patch'),
+    ('system_ext/lib64/libcamera_algoup_jni.xiaomi.so', 'system_ext/lib64/libcamera_mianode_jni.xiaomi.so', 'system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so'): blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so'),
+    'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V5-ndk.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'miuicamera-ruby',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
