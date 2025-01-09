cb8fd14b7b4f041def81141e539d419d0e163706
Arian <arian.kulmer@web.de>
2025-01-09 18:29:00 +0100

    Patch algo jni lib to use correct Surface::connect method
    In android 15 QPR1, google has removed a public method of the Surface class.
    Our jni lib used that method and relies on the offset remaining the same, so
    patch the lib to load it with the smaller offset.
    https://github.com/LineageOS/android_frameworks_native/commit/6a5fdc1d5f6f76c5938aa73f72941bd3f2643628
    
    Change-Id: I97e7ca9f24adad3ede69b599f8d44fdc96646048

--- extract-files.py
+++ extract-files.py
@@ -20,7 +20,10 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
         .apktool_patch('blob-patches/MIUICamera.patch'),
-    ('system_ext/lib64/libcamera_algoup_jni.xiaomi.so', 'system_ext/lib64/libcamera_mianode_jni.xiaomi.so', 'system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so'): blob_fixup()
+    'system_ext/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so')
+        .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
+    ('system_ext/lib64/libcamera_mianode_jni.xiaomi.so', 'system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so'): blob_fixup()
         .add_needed('libgui_shim_miuicamera.so'),
     'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V5-ndk.so'),
